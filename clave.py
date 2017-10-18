c47a9f9f11f550f00be564576a7ef70e66207668
root <cimahuva@gmail.com>
2017-10-18 07:47:18 -0500

    Programa Clave Python - Cinthia Huerta

---
+++ clave.py
@@ -0,0 +1,7 @@
+#-+-coding:utf-8-+-
+clave="secreto"
+entrada=raw_input("Introduce la clave: ")
+if entrada==clave:
+	print "Acertaste, la clave es: ", entrada, ""
+else:
+	print "Acceso no autorizado"
